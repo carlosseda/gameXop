34ab2002f8288a19c7247836d62d177317a51e5f
carlosseda <carlossedagambin@gmail.com>
2024-03-06 22:03:50 +0100

    page generator

--- client/admin/src/components/page-generator.js
+++ client/admin/src/components/page-generator.js
@@ -68,8 +68,8 @@ class PageGenerator extends HTMLElement {
         }
       }
     ]
-    this.uuidSlot = ''
-    this.uuidOptions = ''
+
+    this.component = {}
   }
 
   connectedCallback () {
@@ -638,7 +638,9 @@ class PageGenerator extends HTMLElement {
         const components = this.components.filter(component => component.screenSizes.includes(size))
 
         if (addButton.parentElement.dataset.uuid) {
-          this.uuidSlot = addButton.parentElement.dataset.uuid
+          this.component = {
+            parentUuid: addButton.parentElement.dataset.uuid
+          }
         }
 
         const modal = this.shadow.querySelector('.modal')
@@ -653,7 +655,7 @@ class PageGenerator extends HTMLElement {
         const size = this.shadow.querySelector('.tab-panel.active').dataset.tab
         const editButton = event.target.closest('.edit-button')
         const component = this.components.find(component => component.name === editButton.dataset.component)
-        this.uuidOptions = {
+        this.component = {
           uuid: editButton.dataset.uuid,
           size
         }
@@ -674,6 +676,22 @@ class PageGenerator extends HTMLElement {
 
         this.removeComponent(this.structure[size], uuid)
         console.log(this.mapToObject(this.structure.xs))
+      }
+
+      if (event.target.closest('.row-button')) {
+        const size = this.shadow.querySelector('.tab-panel.active').dataset.tab
+        const rowButton = event.target.closest('.row-button')
+        this.component = {
+          uuid: rowButton.dataset.uuid,
+          size
+        }
+
+        const modal = this.shadow.querySelector('.modal')
+        modal.classList.add('active')
+        const modalContainerBody = this.shadow.querySelector('.modal-container-body')
+        modalContainerBody.innerHTML = ''
+
+        this.showRowOptions(modalContainerBody)
       }
 
       if (event.target.closest('.json-button')) {
@@ -777,7 +795,7 @@ class PageGenerator extends HTMLElement {
     modalContainer.querySelector('.tab-panel').classList.add('active')
     this.renderTabs(modalContainer)
 
-    const optionsValue = this.getOptionsInStructure(this.structure[this.uuidOptions.size], this.uuidOptions.uuid)
+    const optionsValue = this.getOptionsInStructure(this.structure[this.component.size], this.component.uuid)
 
     if (optionsValue) {
       for (const [key, value] of Object.entries(optionsValue)) {
@@ -853,9 +871,9 @@ class PageGenerator extends HTMLElement {
           options[key] = value
         })
 
-        this.updateOptionsInStructure(this.structure[this.uuidOptions.size], this.uuidOptions.uuid, options)
+        this.updateOptionsInStructure(this.structure[this.component.size], this.component.uuid, options)
 
-        this.uuidOptions = {}
+        this.component = {}
         this.shadow.querySelector('.modal').classList.remove('active')
       }
     })
@@ -878,11 +896,19 @@ class PageGenerator extends HTMLElement {
 
     const uuid = Math.random().toString(36).substring(7)
 
+    if (component.name === 'row-component') {
+      const rowButton = document.createElement('button')
+      rowButton.classList.add('row-button')
+      rowButton.dataset.uuid = uuid
+      rowButton.innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><path d='M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z'SSS /></svg>"
+      componentDetailsContainer.append(rowButton)
+    }
+
     const editButton = document.createElement('button')
     editButton.classList.add('edit-button')
     editButton.dataset.component = component.name
     editButton.dataset.uuid = uuid
-    editButton.innerHTML = "<svg viewBox='0 0 24 24'><path fill='currentColor' d='M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z'></path></svg>"
+    editButton.innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><path d='M12,15.5A3.5,3.5 0 0,1 8.5,12A3.5,3.5 0 0,1 12,8.5A3.5,3.5 0 0,1 15.5,12A3.5,3.5 0 0,1 12,15.5M19.43,12.97C19.47,12.65 19.5,12.33 19.5,12C19.5,11.67 19.47,11.34 19.43,11L21.54,9.37C21.73,9.22 21.78,8.95 21.66,8.73L19.66,5.27C19.54,5.05 19.27,4.96 19.05,5.05L16.56,6.05C16.04,5.66 15.5,5.32 14.87,5.07L14.5,2.42C14.46,2.18 14.25,2 14,2H10C9.75,2 9.54,2.18 9.5,2.42L9.13,5.07C8.5,5.32 7.96,5.66 7.44,6.05L4.95,5.05C4.73,4.96 4.46,5.05 4.34,5.27L2.34,8.73C2.21,8.95 2.27,9.22 2.46,9.37L4.57,11C4.53,11.34 4.5,11.67 4.5,12C4.5,12.33 4.53,12.65 4.57,12.97L2.46,14.63C2.27,14.78 2.21,15.05 2.34,15.27L4.34,18.73C4.46,18.95 4.73,19.03 4.95,18.95L7.44,17.94C7.96,18.34 8.5,18.68 9.13,18.93L9.5,21.58C9.54,21.82 9.75,22 10,22H14C14.25,22 14.46,21.82 14.5,21.58L14.87,18.93C15.5,18.67 16.04,18.34 16.56,17.94L19.05,18.95C19.27,19.03 19.54,18.95 19.66,18.73L21.66,15.27C21.78,15.05 21.73,14.78 21.54,14.63L19.43,12.97Z' /></svg>"
     componentDetailsContainer.append(editButton)
 
     const removeButton = document.createElement('button')
@@ -914,16 +940,16 @@ class PageGenerator extends HTMLElement {
 
     const size = tabActive.dataset.tab
 
-    if (this.uuidSlot) {
-      const slot = tabActive.querySelector(`[data-uuid="${this.uuidSlot}"]:not(button)`)
+    if (this.component.parentUuid) {
+      const slot = tabActive.querySelector(`[data-uuid="${this.component.parentUuid}"]:not(button)`)
       slot.before(componentContainer)
-      this.createSlotInStructure(this.structure[size], this.uuidSlot, newComponent.dataset.uuid, component)
+      this.createSlotInStructure(this.structure[size], this.component.parentUuid, newComponent.dataset.uuid, component)
     } else {
       addComponent.before(componentContainer)
       this.createComponentInStructure(this.structure[size], newComponent.dataset.uuid, component)
     }
 
-    this.uuidSlot = ''
+    this.component = {}
   }
 
   removeComponent (structure, uuid) {
